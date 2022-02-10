8f2204d2d71bd72ad7b33af1189a5010c8c9744b
Brant McCarthy <brant.mccarthy@me.com>
2022-02-10 14:36:16 -0600

    added GET function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,5 @@
+// -- DOC READY ---------------------------------------
+
 console.log( 'js' );
 
 $( document ).ready( function(){
@@ -27,14 +29,55 @@ function setupClickListeners() {
   }); 
 }
 
+// -- GLOBAL VARS ----------------------------------
+
+let koalas = [
+  {
+    name: 'testName',
+    age: 'testName',
+    gender: 'testName',
+    readyForTransfer: 'testName',
+    notes: 'testName',
+  },
+  {
+    name: 'testName2',
+    age: 'testName2',
+    gender: 'testName2',
+    readyForTransfer: 'testName2',
+    notes: 'testName2',
+  }
+]
+
+// -- GET -----------------------------------------
+
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+  $.ajax({
+    type: 'GET',
+    url: '/koalas'
+  }).then (function(res){
+    console.log('getKoalas GET successful!');
+    renderKoalas(res);
+  }).catch (function(err){
+    console.log('getKoalas GET Error:', err);
+  });
 } // end getKoalas
+
+// -- POST -----------------------------------------
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
  
 }
+
+// -- PUT ------------------------------------------
+
+
+// -- HELPER FUNCTIONS -----------------------------
+
+// function renderKoalas(koalas){
+//   for (let koala of koalas){
+//   }
+// }
